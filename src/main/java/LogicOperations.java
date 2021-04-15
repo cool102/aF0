8221e9a43274f7bc95c31fee44c9e13e783b7e52
cool102 <68699015+cool102@users.noreply.github.com>
2021-04-15 08:58:32 +0300

    Решил задачу 5

---
+++ src/main/java/LogicOperations.java
@@ -0,0 +1,24 @@
+public class LogicOperations {
+  static boolean doOperationOrThenInvertResult(boolean a, boolean b) {
+    return !(a || b);
+  }
+
+  static boolean doInvertParametrsThenDoOperationAnd(boolean a, boolean b) {
+    return !a && !b;
+  }
+
+  public static void main(String[] args) {
+    System.out.println("Начинает работу первый метод...");
+    System.out.println(doOperationOrThenInvertResult(false, false));
+    System.out.println(doOperationOrThenInvertResult(false, true));
+    System.out.println(doOperationOrThenInvertResult(true, false));
+    System.out.println(doOperationOrThenInvertResult(true, true));
+    System.out.println();
+    System.out.println("Начинает работу второй метод...");
+    System.out.println(doInvertParametrsThenDoOperationAnd(false, false));
+    System.out.println(doInvertParametrsThenDoOperationAnd(false, true));
+    System.out.println(doInvertParametrsThenDoOperationAnd(true, false));
+    System.out.println(doInvertParametrsThenDoOperationAnd(true, true));
+
+  }
+}
