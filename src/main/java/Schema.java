5921db20df60646b78337911817fa2909525f250
cool102 <68699015+cool102@users.noreply.github.com>
2021-04-15 09:07:24 +0300

    Решил задачу 6

---
+++ src/main/java/Schema.java
@@ -0,0 +1,13 @@
+public class Schema {
+  static boolean doAndThenDoNotOrThenDoAnd (boolean a,boolean b, boolean c,boolean d){
+    return (a && b) && !(c||d);
+  }
+
+  public static void main(String[] args) {
+    System.out.println(doAndThenDoNotOrThenDoAnd(false,false,false,false));
+    System.out.println(doAndThenDoNotOrThenDoAnd(true,true,true,true));
+    System.out.println(doAndThenDoNotOrThenDoAnd(false,true,false,true));
+    System.out.println(doAndThenDoNotOrThenDoAnd(true,false,true,false));
+    System.out.println(doAndThenDoNotOrThenDoAnd(true,true,false,false));
+  }
+}
