488a2fdd31d6a0311b57ed228db1b6cae0343855
cool102 <68699015+cool102@users.noreply.github.com>
2021-04-30 16:00:11 +0300

    решил задачу 6 (урок 2)

---
+++ src/main/lesson1/task1/java/Circle.java
@@ -0,0 +1,16 @@
+public class Circle {
+  int radius;
+  final double PI = 3.14;
+
+  double circleSquare(int rad) {
+    radius = rad;
+    return PI * radius * radius;
+  }
+
+  public static void main(String[] args) {
+    Circle circle = new Circle();
+    System.out.println(circle.circleSquare(0));
+    System.out.println(circle.circleSquare(2));
+    System.out.println(circle.circleSquare(100));
+  }
+}
