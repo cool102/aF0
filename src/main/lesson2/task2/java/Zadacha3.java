06ffac9ed1d9dcf11af1346887b4242136eedb02
cool102 <68699015+cool102@users.noreply.github.com>
2021-05-05 14:54:05 +0300

    Исправил замечания по задаче 3 урок 2: 1. Оптимизировал код добавив тернарный оператор 2. Убрал лишние String.valueOf() 3. Проверку на null "вынес вперед" 4. отформатировал код

--- src/main/lesson2/task2/java/Zadacha3.java
+++ src/main/lesson2/task2/java/Zadacha3.java
@@ -1,57 +1,45 @@
-public class Zadacha3<label149> {
+public class Zadacha3 {
 
-  static void isPhoneLengthIsOk1(String phoneNumber){
+  static void isPhoneLengthIsOk1(String phoneNumber) {
     boolean matches = phoneNumber.matches("^[+][7][(]\\d{3}[)]\\d{3}[-]\\d{2}[-]\\d{2}$");
-    if(matches){
-      System.out.println("true1");
-    }
-    else {
-      System.out.println("false1");
-    }
+    System.out.println(matches ? "true1" : "false1");
   }
 
-  static void isPhoneLengthIsOk(String phoneNumber)
-    {
-    if (phoneNumber.length() == 16 && phoneNumber != null)
 
-    {
+  static void isPhoneLengthIsOk(String phoneNumber) {
+    if (phoneNumber != null) {
+      if (phoneNumber.length() == 16) {
 
-      char firstChar;
-      firstChar = phoneNumber.charAt(0);
-      char secondChar;
-      secondChar = phoneNumber.charAt(1);
-      char thirdChar;
-      thirdChar = phoneNumber.charAt(2);
-      char sevenChar;
-      sevenChar = phoneNumber.charAt(6);
-      char elevenChar;
-      elevenChar = phoneNumber.charAt(10);
-      char foortheenChar;
-      foortheenChar = phoneNumber.charAt(13);
+        char firstChar = phoneNumber.charAt(0);
+        char secondChar = phoneNumber.charAt(1);
+        char thirdChar = phoneNumber.charAt(2);
+        char sevenChar = phoneNumber.charAt(6);
+        char elevenChar = phoneNumber.charAt(10);
+        char foortheenChar = phoneNumber.charAt(13);
 
-      if (firstChar == '+' && secondChar == '7' && thirdChar == '(' && sevenChar == ')' && elevenChar == '-' && foortheenChar == '-')
-      abc:{
-        {
-          String sub1;
-          String pattern = "0123456789";
-          sub1 = String.valueOf(phoneNumber.charAt(3)) + String.valueOf(phoneNumber.charAt(4)) + String.valueOf(phoneNumber.charAt(5)) + String.valueOf(phoneNumber.charAt(7)) + String.valueOf(phoneNumber.charAt(8)) + String.valueOf(phoneNumber.charAt(9)) + String.valueOf(phoneNumber.charAt(11) + String.valueOf(phoneNumber.charAt(12)) + String.valueOf(phoneNumber.charAt(14)) + String.valueOf(phoneNumber.charAt(15)));
-          for (int i = 0; i < sub1.length() - 1; i++) {
-            if (!(pattern.contains(String.valueOf(sub1.charAt(i))))) {
-              //System.out.println(sub1.charAt(i));
-              System.out.println("false");
-              break abc;
+        if (firstChar == '+' && secondChar == '7' && thirdChar == '(' && sevenChar == ')' && elevenChar == '-' && foortheenChar == '-')
+          abc:{
+            {
+              String sub1;
+              String pattern = "0123456789";
+              sub1 = String.valueOf(phoneNumber.charAt(3)) + (phoneNumber.charAt(4)) + (phoneNumber.charAt(5)) + (phoneNumber.charAt(7)) + (phoneNumber.charAt(8)) + (phoneNumber.charAt(9)) + (phoneNumber.charAt(11) + String.valueOf(phoneNumber.charAt(12)) + (phoneNumber.charAt(14)) + (phoneNumber.charAt(15)));
+              for (int i = 0; i < sub1.length() - 1; i++) {
+                if (!(pattern.contains(String.valueOf(sub1.charAt(i))))) {
+
+                  System.out.println("false");
+                  break abc;
+                }
+              }
+              System.out.println("true");
             }
           }
-          System.out.println("true");
+        else {
+          System.out.println("false");
         }
-      }
-
-    else {
+      } else {
         System.out.println("false");
       }
-    }
-
-       else {
+    } else {
       System.out.println("false");
     }
   }
@@ -64,6 +52,7 @@ public class Zadacha3<label149> {
     isPhoneLengthIsOk("+5(911)310-12-74");
     isPhoneLengthIsOk("+7(9fg)125-42-99");
     isPhoneLengthIsOk("+7(122)2342343");
+    isPhoneLengthIsOk("");
     System.out.println("__________________________");
     isPhoneLengthIsOk1("+7(912)632-10-64");
     isPhoneLengthIsOk1("7(910)423-73-12");
@@ -71,7 +60,7 @@ public class Zadacha3<label149> {
     isPhoneLengthIsOk1("+5(911)310-12-74");
     isPhoneLengthIsOk1("+7(9fg)125-42-99");
     isPhoneLengthIsOk1("+7(122)2342343");
-
+    isPhoneLengthIsOk1("dsasdfdsfdsfsdfsdfsd");
 
 
   }
