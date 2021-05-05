53fc2a059d243e8b979ceab21a826bb1b0248128
cool102 <68699015+cool102@users.noreply.github.com>
2021-05-05 15:02:42 +0300

    Исправил замечания по задаче 4 урок 2: 1. Изменил имя метода составляющего результирующую строку из совпадающих элментов 2 строк 2. отформатировал код

--- src/main/lesson2/task2/java/Zadacha4.java
+++ src/main/lesson2/task2/java/Zadacha4.java
@@ -2,38 +2,37 @@ import java.util.Arrays;
 
 public class Zadacha4 {
 
-  static void resultString(String s1, String s2) {
+  static void resultStringFromMatchedElements(String s1, String s2) {
 
     boolean equals = s1.equals(s2);
     if (equals) {
       System.out.println("Итоговая строка в которой в которой есть элементы входящие и в первую и во второую строку  = " + s1);
     }
     String pattern = s1;
-    String str12 ="";
+    String str12 = "";
     String str3 = "";
     for (int i = 0; i < s2.length(); i++) {
       if (pattern.contains(String.valueOf(s2.charAt(i)))) {
-        str12+=s2.charAt(i);
-        }
+        str12 += s2.charAt(i);
+      }
     }
 
     for (int i = 0; i < str12.length(); i++) {
       if (str3.contains(String.valueOf(str12.charAt(i)))) {
-      }
-      else str3=str3+str12.charAt(i);
+      } else str3 = str3 + str12.charAt(i);
     }
     //System.out.println(str3);
     char[] str3alphabet = str3.toCharArray();
-    Arrays.sort (str3alphabet);
+    Arrays.sort(str3alphabet);
     System.out.println(str3alphabet);
   }
 
   public static void main(String[] args) {
-    resultString("1213", "1126");
-    resultString("hello", "world");
-    resultString("ping", "pong");
-    resultString("qwer", "asdf");
-    resultString("post", "get");
-    resultString("abs", "sss");
+    resultStringFromMatchedElements("1213", "1126");
+    resultStringFromMatchedElements("hello", "world");
+    resultStringFromMatchedElements("ping", "pong");
+    resultStringFromMatchedElements("qwer", "asdf");
+    resultStringFromMatchedElements("post", "get");
+    resultStringFromMatchedElements("abs", "sss");
   }
 }
