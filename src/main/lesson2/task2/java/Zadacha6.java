45a1ea418cec8be19d618aa8297621fd6f26bdc2
cool102 <68699015+cool102@users.noreply.github.com>
2021-05-05 15:30:12 +0300

    Исправил замечания по задаче 6 урок 2: 1. изменил модификаторы у переменной m и n на final 2. убрал лишний вывод на экран 3. отформатировал код

--- src/main/lesson2/task2/java/Zadacha6.java
+++ src/main/lesson2/task2/java/Zadacha6.java
@@ -5,23 +5,20 @@ public class Zadacha6 {
   public static void main(String[] args) {
 
     System.out.println("создаю массив случайными числами 1 и 0");
-    int m = new Random().nextInt(4) + 7;
-    int n = new Random().nextInt(4) + 7;
+    final int m = new Random().nextInt(4) + 7;
+    final int n = new Random().nextInt(4) + 7;
     int[][] sArray = new int[m][n];
     for (int i = 0; i < m; i++) {
       System.out.println();
       for (int j = 0; j < n; j++) {
         sArray[i][j] = new Random().nextInt(2);
-        System.out.print(sArray[i][j] + " ");
+
       }
     }
 
     //создаю координаты х у в которые запишу число 2
     int x = new Random().nextInt(1) + (m - 2);
     int y = new Random().nextInt(1) + (n - 3);
-    System.out.println();
-    System.out.print(x);
-    System.out.print(y);
     //записал число 2
     sArray[x][y] = 2;
 
@@ -42,21 +39,21 @@ public class Zadacha6 {
     }
 
     System.out.println("печатаю исходный массив");
-    for (int[] row :sArray) {
+    for (int[] row : sArray) {
       System.out.println();
-      for (int i : row )
-              {
-        System.out.print(i+" ");
+      for (int i : row) {
+        System.out.print(i + " ");
       }
     }
-    System.out.println("---------------");
+    System.out.println("");
+    System.out.println("");
+
 
     System.out.println("печатаю инвертированный после числа \"2\" массив");
-    for (int[] row :rArray) {
+    for (int[] row : rArray) {
       System.out.println();
-      for (int i : row )
-      {
-        System.out.print(i+" ");
+      for (int i : row) {
+        System.out.print(i + " ");
       }
 
     }
