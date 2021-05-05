d5d71d9f7cb4b3259ad6c2657550c8c0edfe2793
cool102 <68699015+cool102@users.noreply.github.com>
2021-05-05 10:30:11 +0300

    добавил описание задачи 8

---
+++ src/main/lesson2/task2/java/Zadacha8.java
@@ -0,0 +1,54 @@
+//вывод на печать
+public class Zadacha8 {
+  static void printLetterANTimesWithBreak(int integer) {
+    switch (integer) {
+      case 1:
+        System.out.println("A");
+        break;
+      case 2:
+        System.out.println("AA");
+        break;
+      case 3:
+        System.out.println("AAA");
+        break;
+      case 4:
+        System.out.println("AAAA");
+        break;
+      default:
+        System.out.println("AAAAA");
+    }
+  }
+
+  static void printLetterANTimesWithoutBreak(int integer) {
+    switch (integer) {
+      default:
+        System.out.print("A");
+      case 4:
+        System.out.print("A");
+      case 3:
+        System.out.print("A");
+      case 2:
+        System.out.print("A");
+      case 1:
+        System.out.print("A");
+    }
+    System.out.println();
+  }
+
+  public static void main(String[] args) {
+    printLetterANTimesWithBreak(-1);
+    printLetterANTimesWithBreak(2);
+    printLetterANTimesWithBreak(4);
+    printLetterANTimesWithBreak(7);
+    printLetterANTimesWithBreak(0);
+
+    System.out.println("Второй способ");
+    printLetterANTimesWithoutBreak(-1);
+    printLetterANTimesWithoutBreak(2);
+    printLetterANTimesWithoutBreak(4);
+    printLetterANTimesWithoutBreak(7);
+    printLetterANTimesWithoutBreak(0);
+
+
+  }
+}
