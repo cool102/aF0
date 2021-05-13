9860a55a55e4c90ec42c2b208cd09d7a15f864fb
cool102 <68699015+cool102@users.noreply.github.com>
2021-05-13 14:40:51 +0300

    сделал  задачу №1  урока 3

---
+++ src/main/java/lesson3/task1/Calculator.java
@@ -0,0 +1,53 @@
+package lesson3.task1;
+
+public class Calculator {
+  /**
+   * метод складывает 2 числа
+   *
+   * @param a
+   * @param b
+   * @return
+   */
+  int summ(int a, int b) {
+    int s = a + b;
+    return s;
+  }
+
+  /**
+   * метод вычитает из числа a второе число b
+   *
+   * @param a
+   * @param b
+   * @return
+   */
+  int substract(int a, int b) {
+    int s = a - b;
+    return s;
+
+  }
+
+  /**
+   * метод уменожает 2 числа
+   *
+   * @param a
+   * @param b
+   * @return
+   */
+  int multiply(int a, int b) {
+    int s = a * b;
+    return s;
+
+  }
+
+  /**
+   * метод делит число а на число б, остаток отбрасывает
+   * @param a
+   * @param b
+   * @return
+   */
+  int devide(int a, int b) {
+    int s = a/b;
+    return s;
+
+  }
+}
