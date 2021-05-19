70142ff4b01cf56a57a3416e714f88e2186e28a5
cool102 <68699015+cool102@users.noreply.github.com>
2021-05-19 14:16:51 +0300

    сделал  задачу №4  урока 3

---
+++ src/main/java/lesson3/task4/Human.java
@@ -0,0 +1,52 @@
+package lesson3.task4;
+
+public class Human {
+  String name;
+  Human[] friends = new Human[5];
+
+  /**
+   * создал КОнструктор инициализирующий поле name
+   *
+   * @param nameOfHuman
+   */
+  Human(String nameOfHuman) {
+    this.name = nameOfHuman;
+  }
+
+  /**
+   * создал метод
+   * <p>
+   * добавляет человек 2 в друзья человека 1 и сразу же добавляет человека 1 в друзья человека 2
+   *
+   * @param
+   */
+  public void addFriend(Human friend) {
+    int index = 0;
+    while (friends[index] != null) {
+      if (friends[index] == friend) return;
+      index++;
+    }
+    friends[index] = friend;
+    friend.addFriend(this);
+  }
+
+  /**
+   * метод печатает всех друзей вызвавшего метод
+   */
+  void printAllFriends() {
+    for (int i = 0; i < friends.length; i++) {
+      if (friends[i] != null)
+        System.out.println(friends[i].name);
+
+    }
+
+  }
+
+}
+
+
+
+
+
+
+
