d0769b1381a5e2d9d3df810c7e1364ad3f91ca46
cool102 <68699015+cool102@users.noreply.github.com>
2021-05-25 16:11:44 +0300

    сделал  задачу №2 урока 4

---
+++ src/main/java/lesson4/task2/Validator.java
@@ -0,0 +1,96 @@
+package lesson4.task2;
+
+public class Validator {
+
+  private final String regexString = "^[\\S]+\\s[\\S]+\\s[\\S]+$";
+  String inputString = "строки";
+  String createdObject = "объекта";
+  String array = "массива Human";
+
+  /**
+   * метод валидирует строку на 2 пробела
+   */
+  public boolean StringFormatValidation(String stringFromInput) {
+    //валидна, если в строке 2 пробела
+    //невалидна -вывести сообщение
+    boolean matches = stringFromInput.matches(regexString);
+   /* if (matches) {
+      goodResultOfOperation(inputString);
+    } else {
+      badResultOfOperation(inputString);
+    }*/
+    return matches;
+  }
+
+  /**
+   * метод возвращает массив строк разделением по пробелам
+   */
+  public String[] returnStringSpacedArray(String inputString) {
+    //вывести результат операций
+    return inputString.split(" ");
+  }
+
+  /**
+   * метод валидирует Объект типа Human
+   */
+  public boolean isHumanValid(Human object) {
+
+    boolean isHumanValid = object.getLastName() != null && object.getFirstName() != null && object.getBirthDate() != null;
+   /* if (isHumanValid) {
+      goodResultOfOperation(createdObject);
+
+    } else {
+      badResultOfOperation(createdObject);
+
+    }*/
+    return isHumanValid;
+  }
+
+  /**
+   * метод валидирует массив объектов типа Human
+   */
+  public boolean isArrayOfHumanFull(Human[] arrayOfHuman) {
+
+    boolean isArrayOfHumanFull = true;
+    for (Human current : arrayOfHuman) {
+      if (current == null) {
+        //goodResultOfOperation(array);
+        isArrayOfHumanFull = false;
+        break;
+      }
+    }
+      String output = isArrayOfHumanFull ?
+              "массив полон" :
+              "массив доступен для заполнения";
+      System.out.println(output);
+      return isArrayOfHumanFull;
+
+    }
+  }
+
+
+  /**
+   * метод печатающий положительный результат операции
+   *
+   * @param type
+   * @param string
+   */
+  /*void goodResultOfOperation(String type) {
+    System.out.printf("Валидация  %s успешна", type);
+    System.out.println();
+  }*/
+
+  /**
+   * метод печатающий негативный результат операции
+   *
+   * @param type
+   * @param string
+   */
+
+  /*void badResultOfOperation(String type) {
+    System.out.printf("Валидация %s неуспешна", type);
+    System.out.println();
+  }*/
+
+
+
