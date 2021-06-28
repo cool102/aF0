d055fa391d3861e0a7fe137f5a89b410cbd14deb
cool102 <68699015+cool102@users.noreply.github.com>
2021-06-28 07:44:17 +0300

    добавил не доделанную задачу 2 урок 7 (ООП)

---
+++ src/main/java/lesson7/task2/ThreeHashMap.java
@@ -0,0 +1,11 @@
+package lesson7.task2;
+
+import java.util.HashMap;
+
+public class ThreeHashMap<T extends HashMap > extends HashMap {
+
+
+    public int size() {
+        return super.size();
+    }
+}
