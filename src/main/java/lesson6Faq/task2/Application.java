4a06ed0f4c49c8fc952d5c8b633bcf41ee19aff3
cool102 <68699015+cool102@users.noreply.github.com>
2021-07-06 09:47:55 +0300

    исправил замечание к уроку 6 (задание 2)

--- src/main/java/lesson6Faq/task2/Application.java
+++ src/main/java/lesson6Faq/task2/Application.java
@@ -11,21 +11,25 @@ public class Application {
 
 
         Set<Human> setOfHuman = new HashSet<>();
+
         for (int i = 0; i < 2000; i++) {
             setOfHuman.add(HumanGenerator.generateHuman());
-
         }
-        Iterator<Human> iterartor = setOfHuman.iterator();
-        while (iterartor.hasNext()) {
-            Human human = iterartor.next();
+
+        Iterator<Human> iterator = setOfHuman.iterator();
+        while (iterator.hasNext()) {
+            Human human = iterator.next();
             Set<TypeOfPhone> typeOfPhones = human.getPhonesOfHuman().keySet();
-            if (typeOfPhones.contains(TypeOfPhone.HOMEPHONE) && typeOfPhones.contains(TypeOfPhone.WORKPHONE) && typeOfPhones.contains(TypeOfPhone.MOBILEPHONE)) {
+            if (typeOfPhones.contains(TypeOfPhone.HOMEPHONE)
+                    && typeOfPhones.contains(TypeOfPhone.WORKPHONE)
+                        && typeOfPhones.contains(TypeOfPhone.MOBILEPHONE))
+            {
                 System.out.printf("%s %s,возраст %d, тел: дом %s, раб. %s, моб. %s\n", human.getName(), human.getSurname(), human.getAge(), human.getPhonesOfHuman().get(TypeOfPhone.HOMEPHONE), human.getPhonesOfHuman().get(TypeOfPhone.WORKPHONE), human.getPhonesOfHuman().get(TypeOfPhone.MOBILEPHONE));
             }
-            if (typeOfPhones.contains(TypeOfPhone.HOMEPHONE) || typeOfPhones.contains(TypeOfPhone.WORKPHONE) || typeOfPhones.contains(TypeOfPhone.MOBILEPHONE))
-                iterartor.remove();
-
-
+            if (typeOfPhones.contains(TypeOfPhone.HOMEPHONE)
+                    || typeOfPhones.contains(TypeOfPhone.WORKPHONE)
+                        || typeOfPhones.contains(TypeOfPhone.MOBILEPHONE))
+                iterator.remove();
         }
         System.out.println("Количество людей после удаления" + setOfHuman.size());
 
