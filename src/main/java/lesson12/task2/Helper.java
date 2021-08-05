75884ac5875747fde6193084800b8a74f2103275
cool102 <68699015+cool102@users.noreply.github.com>
2021-08-05 18:15:32 +0300

    сделал задание  к уроку 12 (задание 2)

---
+++ src/main/java/lesson12/task2/Helper.java
@@ -0,0 +1,31 @@
+package lesson12.task2;
+
+public class Helper {
+
+    private String path = "src/main/java/lesson12/task2/data.xlsx";
+
+
+
+   //public static String getJsonArrayFromXls(String path, String sheetName) throws IOException {
+   //    //открыть эксель файл
+   //    // открыть лист
+   //    // считать первую ячейку строки 1- это будет имя поле объекта с типом лист<String>
+   //    //создать конструктор объекта,
+   //    // проинициализировать каждое поле значениями соответственно из столбца
+   //    //сериализовать объект в Json
+
+
+   //    FileInputStream excelFile = new FileInputStream(new File(path));
+   //    HSSFWorkbook workbook = new HSSFWorkbook(excelFile);
+   //    HSSFSheet sheet = workbook.getSheet(sheetName);
+   //    Row firstRow = sheet.getRow(0);
+   //    String key1 = firstRow.getCell(0).getStringCellValue();
+   //    List<String> value1 = new ArrayList<>();
+   //    value1.add(sheet.getRow(1).getCell(0).getStringCellValue());
+
+
+
+
+   //}
+
+}
