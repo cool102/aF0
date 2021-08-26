92a8b5dab81780adf380216c5c11963217d77ab4
cool102 <68699015+cool102@users.noreply.github.com>
2021-08-26 16:14:51 +0300

    Сделал задание к уроку 14-15 (задания 0,1,2,3,4,5)

---
+++ src/main/java/lesson14/task0/model/Passport.java
@@ -0,0 +1,72 @@
+package lesson14.task0.model;
+
+public class Passport {
+    private String series;
+    private String number;
+    private String issuer;
+    private String issueDate;
+    private String issuerCode;
+
+
+    public Passport(String series, String number, String issuer, String issueDate, String issuerCode) {
+        this.series = series;
+        this.number = number;
+        this.issuer = issuer;
+        this.issueDate = issueDate;
+        this.issuerCode = issuerCode;
+    }
+
+    public Passport() {
+    }
+
+    @Override
+    public String toString() {
+        return "Passport{" +
+                "series='" + series + '\'' +
+                ", number='" + number + '\'' +
+                ", issuer='" + issuer + '\'' +
+                ", issueDate='" + issueDate + '\'' +
+                ", issuerCode='" + issuerCode + '\'' +
+                '}';
+    }
+
+    public String getSeries() {
+        return series;
+    }
+
+    public void setSeries(String series) {
+        this.series = series;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public String getIssuer() {
+        return issuer;
+    }
+
+    public void setIssuer(String issuer) {
+        this.issuer = issuer;
+    }
+
+    public String getIssueDate() {
+        return issueDate;
+    }
+
+    public void setIssueDate(String issueDate) {
+        this.issueDate = issueDate;
+    }
+
+    public String getIssuerCode() {
+        return issuerCode;
+    }
+
+    public void setIssuerCode(String issuerCode) {
+        this.issuerCode = issuerCode;
+    }
+}
